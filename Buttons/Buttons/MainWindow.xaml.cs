64ee90b7add90139eaf896d6911164fd4c882b76
Nglsnkrrs <ternavsdop@gmail.com>
2025-10-10 17:34:49 +0300

    Buttons

---
+++ Buttons/Buttons/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace ColorButtonsDemo
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            foreach (var child in ColorButtonsPanel.Children)
+            {
+                if (child is Button button)
+                {
+                    SetButtonColor(button);
+                }
+            }
+
+            UpdateContainerWidth();
+        }
+
+        private void SetButtonColor(Button button)
+        {
+            string colorName = button.Content.ToString();
+
+            try
+            {
+                var prop = typeof(Colors).GetProperty(colorName);
+                if (prop != null)
+                {
+                    Color color = (Color)prop.GetValue(null);
+                    button.Foreground = new SolidColorBrush(color);
+                }
+                else
+                {
+                    button.Foreground = Brushes.Black;
+                }
+            }
+            catch
+            {
+                button.Foreground = Brushes.Black;
+            }
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            UpdateContainerWidth();
+        }
+
+        private void UpdateContainerWidth()
+        {
+            ButtonsContainer.Width = this.ActualWidth * 2 / 3;
+        }
+    }
+}
